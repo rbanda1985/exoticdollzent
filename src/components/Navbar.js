4a2e2f8674445fea2f5bbb6e5e9849a9bb89caf8
rbanda1985 <robertbanda85@gmail.com>
2022-05-06 17:35:18 -0500

    CHNGE: Navbar styling and links

---
+++ src/components/Navbar.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import './Navbar.css';
+import {
+  MDBNavbar,
+  MDBContainer,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBNavbarToggler,
+  MDBCollapse,
+} from 'mdb-react-ui-kit';
+
+const Navbar = () => {
+  const [showNavCentred, setShowNavCentred] = useState(false);
+  return (
+    <MDBNavbar expand='lg' light bgColor='light'>
+      <MDBContainer fluid>
+        <MDBNavbarToggler
+          type='button'
+          data-target='#navbarCenteredExample'
+          aria-controls='navbarCenteredExample'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+          onClick={() => setShowNavCentred(!showNavCentred)}
+        >
+          <MDBIcon icon='bars' fas />
+        </MDBNavbarToggler>
+
+        <MDBCollapse navbar show={showNavCentred} center id='navbarCenteredExample'>
+          <MDBNavbarNav fullWidth={false} className='mb-2 mb-lg-0'>
+            <MDBNavbarItem className='navbar-item'>
+              <MDBNavbarLink active aria-current='page' href='#'>
+                Home
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem className='navbar-item'>
+              <MDBNavbarLink active aria-current='page' href='#'>
+                Party Packages
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem className='navbar-item'>
+              <MDBNavbarLink active aria-current='page' href='#'>
+                Book Now
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem className='navbar-item'>
+              <MDBNavbarLink active aria-current='page' href='#'>
+                About Us
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem className='navbar-item'>
+              <MDBNavbarLink active aria-current='page' href='#'>
+                Contact Us
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem className='navbar-item'>
+              <MDBNavbarLink active aria-current='page' href='#'>
+                Careers
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+  )
+}
+
+export default Navbar
