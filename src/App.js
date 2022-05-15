1e1cab816ded55267c586c33871a77b1dff0ad3b
rbanda1985 <robertbanda85@gmail.com>
2022-05-15 06:00:57 -0500

    ADD: Page components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import {Routes, Route} from 'react-router-dom'
 import './App.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import PartyPackages from './components/PartyPackages';
+import Booking from './components/Booking';
+import About from './components/About';
+import Contact from './components/Contact';
+import Careers from './components/Careers';
 
 function App() {
   return (
@@ -9,6 +14,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/partypackages' element={<PartyPackages />} />
+        <Route path='/bookings' element={<Booking />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/careers' element={<Careers />} />
       </Routes>
     </div>
   );
---
+++ src/components/PartyPackages.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PartyPackages = () => {
+  return (
+    <div>PartyPackages</div>
+  )
+}
+
+export default PartyPackages
