a1276a34296d20f06f3bb2adf0b76fdad66d3268
rbanda1985 <robertbanda85@gmail.com>
2022-05-15 05:35:09 -0500

    ADD: Styling to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import './Home.css'
 
+
 const Home = () => {
   return (
     <div className='mainHome'>
       <div className='container'>
-        
+        <div className='banner-animation'>
+          <p>Let your fantasies run wild</p>
+        </div>
+        <div className='intro'>
+          <p>
+            Welcome To Exotic Dollz Entertainment
+          </p>
+          <p>
+
+          </p>
+        </div>
+        </div>
       </div>
-      
-    </div>
   )
 }
 
--- src/components/ImageBanner.js
+++ src/components/ImageBanner.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import './ImageBanner.css'
 
-const ImageBanner = ({image}) => {
+const ImageBanner = ({image, textOne, textTwo}) => {
   return (
     <div className='bg-image'>
       <img src={image} className='img-fluid' alt='' />
       <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
-        <div className='d-flex justify-content-center align-items-center h-100'>
-          <p className='text-white mb-0'>Can you see me?</p>
+        <div className='d-flex justify-content-center align-items-center h-100 banner-text'>
+          <p className='text-white mb-0'>{textOne}</p>
+          <p className='text-white mb-0'>{textTwo}</p>
         </div>
       </div>
     </div>
