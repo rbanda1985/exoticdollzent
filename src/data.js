bc73ae8ed3b08ec1e9f51567644d43a6ba96c198
rbanda1985 <robertbanda85@gmail.com>
2022-05-21 16:54:55 -0500

    ADD: data.js

---
+++ src/data.js
@@ -0,0 +1,49 @@
+const imageData = [
+  {
+    image: require('./assets/bachelor-party.jpg'),
+    alt: 'Bachelor Party',
+    label: 'Bachelor Parties'
+  },
+  {
+    image: require('./assets/birthday.jpg'),
+    alt: 'Birthday Party',
+    label: 'Birthday Parties'
+  },
+  {
+    image: require('./assets/going-away.jpg'),
+    alt: 'Going Away Party',
+    label: 'Going Away Parties'
+  },
+  {
+    image: require('./assets/golf.jpg'),
+    alt: 'Golf Outing',
+    label: 'Golf Outings'
+  },
+  {
+    image: require('./assets/holiday.jpg'),
+    alt: 'Holiday Party',
+    label: 'Holiday Parties'
+  },
+  {
+    image: require('./assets/office-party.jpg'),
+    alt: 'Office Party',
+    label: 'Office Parties'
+  },
+  {
+    image: require('./assets/poker.jpg'),
+    alt: 'Poker Party',
+    label: 'Poker Parties'
+  },
+  {
+    image: require('./assets/sporting.jpg'),
+    alt: 'Sporting Event',
+    label: 'Sporting Events'
+  },
+  {
+    image: require('./assets/strip-yacht.jpg'),
+    alt: 'Boat Party',
+    label: 'Yacht Parties'
+  }
+]
+
+export default imageData;
