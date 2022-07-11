a3f1c9cbe33f7bf6fbd5cb26400cd1f0968eb28d
rbanda1985 <robertbanda85@gmail.com>
2022-07-11 06:04:46 -0500

    ADD: Redirect folder

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ const About = () => {
     <div className='about'>
       <TopImage image={image} title='About Us' />
       <div className='about-us-container'>
-        <p>Exotic Dollz and Entertainmnet</p> <p>is proud to offer Dallas with the sexiest and hottest female exotic dancers and topless/nude waitresses</p>
+        <p>Exotic Dollz Entertainmnet</p> <p>is proud to offer Dallas with the sexiest and hottest female exotic dancers and topless/nude waitresses</p>
         <p>We offer Bachelor Parties - Boat Parties - Golf Outings - Poker Parties - Sporting Events - Going Away Parties - Divorce Parties - Office Parties - Holiday Parties - Birthday Parties or any kind of event you may have.</p>
         <p>We have party packages available that will leave you worry free for the entire evening. Shows may include topless/nude waitresses, single girl strips, single girl fantasy shows, 2 and 3 girl fantasy shows, slip and slide, feed the kitty and an assortment of party tricks that you and your guests will never forget.</p>
         <p>We have years of experience in the business, so rest assured we know what our clients LIKE and WANT!! Do not be fooled by other agencies claiming to be the biggest. GO BIG OR GO HOME we are the best! Our girls are classy and friendly and will make sure that your party is a success!!</p>
